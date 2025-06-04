9a1a4b32b01aae6d999b4c3e547e267da9ba9097
DESKTOP-2OSFLH9\miuli <miuliana959@gmail.com>
2025-06-04 03:40:27 +0300

    # login validation + generate token

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using GymFit.Data;
+using GymFit.DTOs;
+using GymFit.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+namespace GymFit.Controllers
+   
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+        public AuthController(AuthService authService)
+        {
+            _authService = authService; 
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            var user = _authService.ValidateLogin(request.Email, request.Password);
+            if(user == null)
+            {
+                return Unauthorized("Incorrect email or password.");
+            }
+
+            var token = _authService.GenerateJwtToken(user);
+            return Ok(new
+            {
+                token = token,
+                user = new
+                {
+                    id = user.Id,
+                    name = $"{user.FirstName},{user.LastName}",
+                    email = user.Email,
+                    role = user.UserType.ToString()
+                }
+
+
+            });
+        }
+
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,60 @@
+﻿using GymFit.Data;
+using GymFit.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace GymFit.Services
+{
+    public class AuthService
+    {
+        private readonly GymFitContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(GymFitContext context,IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public User? ValidateLogin(string email, string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email == email);
+            if(user == null || user.PasswordHash != password)
+            {
+                return null;
+            }
+            return user;
+
+
+        }
+
+        public String GenerateJwtToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("id", user.Id.ToString()),
+                new Claim("email",user.Email),
+                new Claim("name", $"{user.FirstName}, {user.LastName}"),
+                new Claim("role", user.UserType.ToString())
+            };
+
+            var secretKey = _configuration["JwtSettings:SecretKey"];
+            var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+            var securityKey = new SymmetricSecurityKey(keyBytes);
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2), 
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+
+        }
+    }
+}
